21dd6d7f7c159aed644c4489cae7ccd1edf81a79
Apprentice <apprentice@dbc-29.local>
2013-11-07 11:33:00 -0800

    intercepted todo with AJAX

--- public/js/application.js
+++ public/js/application.js
@@ -1,7 +1,10 @@
 $(document).ready(function() {
-  // This is called after the document has loaded in its entirety
-  // This guarantees that any elements we bind to will exist on the page
-  // when we try to bind to them
+  console.log("hi fans")
+  $("#todo_form").on('click', function(e) {
+    console.log("im heeeeeere")
+    e.preventDefault()
+    var todo = $(this).serialize()
+    console.log(todo)
+  })
 
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
 });

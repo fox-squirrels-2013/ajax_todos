4150cbfaaa0a76448c619968f704bb0ce4cea23b
Apprentice <apprentice@dbc-06.local>
2013-11-07 12:34:10 -0800

    Not working, will work on later.

--- public/js/application.js
+++ public/js/application.js
@@ -13,6 +13,20 @@ $(document).ready(function() {
       console.log('fail!')
     })
   })
+
+  $('.submit_completed').on('click', function(e) {
+    e.preventDefault()
+    // var form_data = $(this.serialize)
+    // console.log(form_data)
+    console.log($('.available-todos').find('button').attr('id'))
+    $.ajax({
+      url: '/todos/???/complete',
+      type: 'post',
+      data: {submit: true}
+    }).done(function(server_data) {
+      console.log(server_data)
+    })
+  })
   // This is called after the document has loaded in its entirety
   // This guarantees that any elements we bind to will exist on the page
   // when we try to bind to them

bc4b0e36f5bdbb0abb67ff671aecd58a489fd56d
Apprentice <apprentice@dbc13.local>
2013-11-07 20:10:04 -0800

    morning exercise

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -7,8 +7,13 @@ end
 post '/todos' do
   @todo = Todo.create(params[:todo])
   if request.xhr?
-  # This is how to tell if a request came in over AJAX or not
+  # # This is how to tell if a request came in over AJAX or not
+  # "<li id='todo-<%=@todo.id%>'><%=@todo.task%></li>"
+  # erb :_todo, layout: false - need to create this
 
+
+  # Return JSON
+  @todo.task
   else
     redirect '/'
   end
--- public/js/application.js
+++ public/js/application.js
@@ -1,7 +1,35 @@
 $(document).ready(function() {
-  // This is called after the document has loaded in its entirety
+ $('form').on('submit', function(e) {
+  e.preventDefault();
+  var form_data= $(this).serialize();
+  $.ajax({                    //5-9 is sending data
+    url: this.action,
+    type: this.method,
+    data: $(this).serialize()
+  }).done(function(server_data){      // need this to process data
+    console.log("hi", server_data)
+    $( ".completed-todos" ).after(server_data)
+
+  });
+ });
+});
+
+//index is server app.js is client (sinatra is server)
+
+// $('form').on('submit', function(e){
+// //   e.preventDefault();
+//    $( ".completed-todos" ).after("<li id="todo-<%=todo.id%>"><%=todo.task%></li>" );
+//   $.ajax({
+//     url: "/todos/:id/complete",
+//     type: "post"
+   
+//   })
+// })
+
+
+ // This is called after the document has loaded in its entirety
   // This guarantees that any elements we bind to will exist on the page
   // when we try to bind to them
 
   // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
-});
+

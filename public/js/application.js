17967e178d5c10f91c08b29cbbf28a8709ea59ca
raorao <savinirs@gmail.com>
2013-11-07 21:01:41 -0800

    todos being saved to DB (again)

--- public/js/application.js
+++ public/js/application.js
@@ -1,10 +1,17 @@
 $(document).ready(function() {
-  console.log("hi fans")
+  // using todo_form here binds the on click to the whole add a todo form rather than just the Add Todo click button, but changing it to the id new_todo_task of the button breaks the console.log(todo)
+
   $("#todo_form").on('click', function(e) {
-    console.log("im heeeeeere")
     e.preventDefault()
     var todo = $(this).serialize()
-    console.log(todo)
+    console.log("hello" + todo)
+    $.ajax({
+      type: 'post',
+      url: '/todos',
+      data: todo
+    }).done(function(server_data){
+      console.log(server_data)
+    })
   })
 
 });

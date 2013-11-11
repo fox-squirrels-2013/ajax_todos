cdc53b8340a5ca589c456e193e647157d49622ba
Apprentice <apprentice@dbc-04.local>
2013-11-10 18:31:30 -0800

    completed function working; still need to add tests

--- public/js/application.js
+++ public/js/application.js
@@ -9,8 +9,25 @@ $(document).ready(function() {
       url: '/todos',
       data: todo
     }).done(function(server_data){
-      $("#todo_listing").append(server_data)
+      var todo_form_item = '<div id="todo-' + server_data.id + '">' + server_data.task + '<form method="POST" action="/todos/' + server_data.id + '/complete"><button>Complete!</button></form></div>'
+      $('.available-todos').append(todo_form_item)
     })
+  })
+
+  $(".available-todos").on('submit', '.active-todo', function(e) {
+     e.preventDefault()
+     var new_route = $(this).attr('action')
+     var clicked_item = $(this).parent()
+     $.ajax({
+        type: 'post',
+        url: new_route
+     }).done(function(server_data){
+        console.log(server_data.task)
+        completed_item = '<li id="todo-' + server_data.id + '">' + server_data.task + '</li>'
+        $('.completed-todos').append(completed_item)
+        clicked_item.remove()
+     })
+
   })
 
 });
